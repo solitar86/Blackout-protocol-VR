3bcd5238a9f067141a02edcf0d34caa34a41f477
Iiro <iiro.peltonen@gmail.com>
2026-01-29 17:03:14 +0200

    Added several audiofiles

--- Assets/_Audio/AudioHolders/TouchSoundHolderSO.cs
+++ Assets/_Audio/AudioHolders/TouchSoundHolderSO.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New SoundHolder", menuName = "Audio Holders/New Touch SoundHolder")]
 public class TouchSoundHolderSO : ScriptableObject
 {
+    //TODO Refactor this to use soundholders instead.
     public Sound FirstTouchSound;
     public Sound SlideTouchSound;
     public Sound EndTouchSound;
