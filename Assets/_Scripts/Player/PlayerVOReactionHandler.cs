9a3ff8ee840fdd1552babfc578a029c57c02f26c
Iiro <iiro.peltonen@gmail.com>
2026-02-11 10:05:23 +0200

    Moved Inner monologue calls to a single function

--- Assets/_Scripts/Player/PlayerVOReactionHandler.cs
+++ Assets/_Scripts/Player/PlayerVOReactionHandler.cs
@@ -11,46 +11,38 @@ public class PlayerVOReactionHandler : MonoBehaviour
     [SerializeField] private SoundArrayHolder _curseWordsVO;
 
     private float _turnVODelay = 0.25f;
+    private float _curseDelay = 1.5f;
 
     #region Unity Callbacks
-    void Start()
+    private void Start()
     {
         EventManager.OnPlayerCurse.AddListener(this, PlayerSayCurseWord);
+        EventManager.OnPlayerObjectIDVOShouldPlay.AddListener(this, PlayTouchIDVoiceLine);
         SnapTurnProvider.OnPlayerSnapTurn += HandlePlayerTurn;
     }
     private void OnDisable()
     {
         EventManager.OnPlayerCurse.RemoveListener(this, PlayerSayCurseWord);
+        EventManager.OnPlayerObjectIDVOShouldPlay.RemoveListener(this, PlayTouchIDVoiceLine);
         SnapTurnProvider.OnPlayerSnapTurn -= HandlePlayerTurn;
     }
 
     #endregion
+    
     private void HandlePlayerTurn(bool isRightTurn)
     {
         if (isRightTurn == false)
         {
             if (_leftTurnVO != null && _leftTurnVO.SoundArray != null && _leftTurnVO.SoundArray.Length > 0)
             {
-                this.CallWithDelay(() =>
-                {
-                    AudioPlayer.PlayRandomSoundFromArrayAtPoint(this,
-                                                                _leftTurnVO.SoundArray,
-                                                                transform.position,
-                                                                _curseWordsVO.LastPlayedSound);
-                }, _turnVODelay);
+                PlayPlayerInnerMonologueWithDelay(_leftTurnVO, _turnVODelay);
             }
             return;
         }
 
         if (_rightTurnVO != null && _rightTurnVO.SoundArray != null && _rightTurnVO.SoundArray.Length > 0)
         {
-            this.CallWithDelay(() =>
-            {
-                AudioPlayer.PlayRandomSoundFromArrayAtPoint(this,
-                                                            _rightTurnVO.SoundArray,
-                                                            transform.position,
-                                                            _rightTurnVO.LastPlayedSound);
-            }, _turnVODelay);
+            PlayPlayerInnerMonologueWithDelay(_rightTurnVO, _turnVODelay);
         }
 
     }
@@ -58,10 +50,38 @@ public class PlayerVOReactionHandler : MonoBehaviour
     {
         if (_curseWordsVO != null && _curseWordsVO.SoundArray != null && _curseWordsVO.SoundArray.Length > 0)
         {
-            AudioPlayer.PlayRandomSoundFromArrayAtPoint(this,
-                                                        _curseWordsVO.SoundArray,
-                                                        transform.position,
-                                                        _curseWordsVO.LastPlayedSound);
+            PlayPlayerInnerMonologueWithDelay(_curseWordsVO, _curseDelay);
         }
+    }
+    private void PlayTouchIDVoiceLine(Sound IDVOSound)
+    {
+        PlayPlayerInnerMonologueWithDelay(IDVOSound, PlayerSettings.Developer.IdentifyVODelay);
+    }
+    private void PlayPlayerInnerMonologueWithDelay(SoundArrayHolder soundHolder, float delay)
+    {
+        this.CallWithDelay(() =>
+        {
+            bool spatialize = false;
+            bool pitchVary = false;
+            AudioPlayer.PlayRandomSoundFromArrayAtPoint(this,
+                                                        soundHolder.SoundArray,
+                                                        transform.position,
+                                                        soundHolder.LastPlayedSound,
+                                                        pitchVary,
+                                                        spatialize);
+        }, delay);
+    }
+    private void PlayPlayerInnerMonologueWithDelay(Sound soundToPlay, float delay)
+    {
+        this.CallWithDelay(() =>
+        {
+            bool spatialize = false;
+            bool pitchVary = false;
+            AudioPlayer.PlaySoundAtPoint(this,
+                                        soundToPlay,
+                                        transform.position,
+                                        pitchVary,
+                                        spatialize);
+        }, delay);
     }
 }
