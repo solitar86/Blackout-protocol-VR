742d62106d65cc57c7e8eb4beceb87e8fe9c5aea
Iiro <iiro.peltonen@gmail.com>
2026-06-01 11:00:47 +0300

    Added location ID in tutorial for sink and radio

--- Assets/_Scripts/Systems/LocationVOTriggerVolume.cs
+++ Assets/_Scripts/Systems/LocationVOTriggerVolume.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Audio;
 public class LocationVOTriggerVolume : MonoBehaviour
@@ -25,4 +26,40 @@ public class LocationVOTriggerVolume : MonoBehaviour
         }
 
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        var collider = GetComponent<Collider>();
+
+        if (collider == null) return;
+
+        Transform t = collider.transform;
+
+        // Save matrix
+        Matrix4x4 oldMatrix = Gizmos.matrix;
+
+        // Match collider transform
+        Gizmos.matrix = Matrix4x4.TRS(t.position, t.rotation, t.lossyScale);
+
+        if (collider is BoxCollider box)
+        {
+            Gizmos.color = new Color(0.5f, 0f, 1f, 0.25f);
+            Gizmos.DrawCube(box.center, box.size);
+        }
+        else if (collider is SphereCollider sphere)
+        {
+            Gizmos.color = new Color(0.5f, 0f, 1f, 0.9f);
+            Gizmos.DrawWireSphere(sphere.center, sphere.radius);
+        }
+
+        // Restore matrix
+        Gizmos.matrix = oldMatrix;
+
+#if UNITY_EDITOR
+        // Draw label at the collider object's position
+        Handles.Label(t.position, collider.gameObject.name);
+#endif
+
+    }
 }
+
